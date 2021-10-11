eeee947c178af8cd193b1ee01e523fef12e46c0e
Arip Malik <47932685+Sakuratan99@users.noreply.github.com>
2021-10-11 15:43:22 +0700

    Solitaire
    Learn make game from video Megalomobile

---
+++ Assets/Script/ScoreKeeper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public GameObject HighScorePanel;
+    public Selectable[] topStacks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasWon())
+        {
+            Win();
+        }
+    }
+
+    public bool HasWon()
+    {
+        int i = 0;
+        foreach (Selectable topstack in topStacks)
+        {
+            i += topstack.value;
+        }
+        if(i>= 52)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Win()
+    {
+        HighScorePanel.SetActive(false);
+        print("You Have Won");
+    }
+
+}
---
+++ Assets/Script/UIButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    public GameObject HighScorePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayAgain()
+    {
+        HighScorePanel.SetActive(false);
+        ResetScene();
+    }
+    public void ResetScene()
+    {
+        //find all the cards and remove them
+        UpdateSprite[] cards = FindObjectsOfType<UpdateSprite>();
+        foreach(UpdateSprite card in cards)
+        {
+            Destroy(card.gameObject);
+        }
+        ClearTopValues();
+        //deal new cards 
+        FindObjectOfType<Solitaire>().PlayCards();
+    }
+
+    void ClearTopValues()
+    {
+        Selectable[] selectables = FindObjectsOfType<Selectable>();
+        foreach(Selectable selectable in selectables)
+        {
+            if (selectable.CompareTag("Top"))
+            {
+                selectable.suit = null;
+                selectable.value = 0;
+            }
+        }
+    }
+}
